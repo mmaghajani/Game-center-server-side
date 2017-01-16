4c8f7cf805a193ccc1bb4ca133efd3ad4b0660c3
mahdi aghajani <mma137421@gmail.com>
2017-01-16 19:45:00 +0330

    Completes header API

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,36 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Game;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     //
-    function header(){
+    public function header($title){
+        $game = Game::with('categories')->where( 'title' , '=' , $title)->get();
+
+        $result = ["game" => $game[0]];
+        $response = ["ok" => true, "result" => $result];
+        $final = ["response" => $response];
+        return $final;
+    }
+
+    public function infoTab(){
 
     }
 
-    function infoTab(){
+    public function leaderBoardTab(){
 
     }
 
-    function leaderBoardTab(){
+    public function commentsTab(){
 
     }
 
-    function commentsTab(){
+    public function commentsOffset($offset){
 
     }
 
-    function commentsOffset($offset){
+    public function relatedGamesTab(){
 
     }
 
-    function relatedGamesTab(){
-
-    }
-
-    function galleryTab(){
+    public function galleryTab(){
 
     }
 }
